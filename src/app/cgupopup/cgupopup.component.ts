8f9d4e9c40c3e44bc4bc6a0c99f307634e2584b5
pboucher44 <boucher.pierre44400@hotmail.fr>
2021-03-13 11:22:28 +0100

    First commit

---
+++ src/app/cgupopup/cgupopup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cgupopup',
+  templateUrl: './cgupopup.component.html',
+  styleUrls: ['./cgupopup.component.scss']
+})
+export class CgupopupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
